ece37d586fd3c06e129d9900e9020e8904225d80
juliovianadeoliveira-source <juliovianadeoliveira@gmail.com>
2026-08-22 19:48:32 -0300

    Adiciona configuração central do Web Player

---
+++ config.js
@@ -0,0 +1,10 @@
+window.IPTV_CONFIG = {
+  appName: "IPTV Player",
+  version: "2026.08.22",
+  requestTimeoutMs: 12000,
+  playbackTimeoutMs: 8000,
+  defaultLiveExtension: "ts",
+  preferHttpsOnGithubPages: true,
+  pageSizeChannels: 120,
+  debug: false
+};

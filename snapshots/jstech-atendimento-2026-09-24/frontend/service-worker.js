27b1074dc787185d803d43f25c129b1ab1649d9a
juliovianadeoliveira-source <juliovianadeoliveira@gmail.com>
2026-09-24 18:44:12 -0300

    Snapshot frontend service-worker.js

---
+++ snapshots/jstech-atendimento-2026-09-24/frontend/service-worker.js
@@ -0,0 +1,62 @@
+const CACHE="jstech-atendimento-v11-panel-stability";
+const CORE=[
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./crm.css",
+  "./sync-sources.css",
+  "./app.js",
+  "./crm.js",
+  "./sync-sources.js",
+  "./resellers.js",
+  "./external-clients.js",
+  "./manifest.webmanifest",
+  "./app-icon.svg"
+];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE).catch(()=>{})));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",event=>{
+  const req=event.request;
+  if(req.method!=="GET")return;
+  const url=new URL(req.url);
+  if(url.origin!==self.location.origin)return;
+
+  if(req.mode==="navigate"){
+    event.respondWith(
+      fetch(req)
+        .then(res=>{
+          const copy=res.clone();
+          caches.open(CACHE).then(cache=>cache.put("./index.html",copy)).catch(()=>{});
+          return res;
+        })
+        .catch(()=>caches.match("./index.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(req)
+      .then(res=>{
+        if(res.ok){
+          const copy=res.clone();
+          caches.open(CACHE).then(cache=>cache.put(req,copy)).catch(()=>{});
+        }
+        return res;
+      })
+      .catch(async()=>{
+        return (await caches.match(req,{ignoreSearch:true})) || Response.error();
+      })
+  );
+});
